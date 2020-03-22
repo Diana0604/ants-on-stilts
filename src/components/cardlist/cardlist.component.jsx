9be0fda9570e78724ce66e7bfb9623829d321481
Diana0604 <diana.vallverdu@gmail.com>
2020-03-22 16:56:47 +0000

    rebase

--- src/components/cardlist/cardlist.component.jsx
+++ src/components/cardlist/cardlist.component.jsx
@@ -5,12 +5,16 @@ import Card from '../card/card.component';
 import './cardlist.styles.scss';
 
 const CardList = (props) => (
-  <div className = "card-list">
-    {
-        props.elements.map(element => (
-        <Card element={element} key={element.key} ></Card>
-        ))
-    }
+  <div className="card-list-wrapper">
+    <h1 className="title">{props.title}</h1>
+    <hr></hr>
+    <div className = "card-list">
+      {
+          props.elements.map(element => (
+          <Card element={element} key={element.key} ></Card>
+          ))
+      }
+    </div>
   </div>  
 );
 
