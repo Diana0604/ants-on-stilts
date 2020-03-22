d9af09bb365662f0d1e4215b797e0e6f27b362d6
Diana0604 <diana.vallverdu@gmail.com>
2020-03-22 18:53:11 +0000

    first demo router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 //import logo from './logo.png';
 import './App.css';
 
 import HomePage from './pages/homepage/hompeage.component';
 
+const QrumpetPage = () => (
+  <div>
+    <h1>QRUMPET PAGE</h1>
+  </div>
+)
+
 class App extends Component {
   render () {
     return (
       <div>
-        <HomePage />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/qrumpet" component={QrumpetPage}/>
       </div>
     );
   }
