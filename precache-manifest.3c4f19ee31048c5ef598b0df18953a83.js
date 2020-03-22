0c9462356b4c413fb781adcc50adfff6428bb623
Diana0604 <diana.vallverdu@gmail.com>
2020-03-22 11:48:04 +0000

    Updates

---
+++ precache-manifest.3c4f19ee31048c5ef598b0df18953a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d4a9e0e6311e2b17294904be629796",
+    "url": "/Diana0604/ants-on-stilts/index.html"
+  },
+  {
+    "revision": "8c297c58b35098c13c19",
+    "url": "/Diana0604/ants-on-stilts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5094cd7db10c25d6654a",
+    "url": "/Diana0604/ants-on-stilts/static/js/2.6f26d44d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Diana0604/ants-on-stilts/static/js/2.6f26d44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c297c58b35098c13c19",
+    "url": "/Diana0604/ants-on-stilts/static/js/main.87d010a9.chunk.js"
+  },
+  {
+    "revision": "ed167276d82b5b894f53",
+    "url": "/Diana0604/ants-on-stilts/static/js/runtime-main.4bf65bb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Diana0604/ants-on-stilts/static/media/logo.5d5d9eef.svg"
+  }
+]);
