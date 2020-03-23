40261ed0bf53f72d131402046c9c42bc80c730e9
Diana0604 <diana.vallverdu@gmail.com>
2020-03-23 16:07:39 +0000

    homepage data treatement

--- src/pages/homepage/hompeage.component.jsx
+++ src/pages/homepage/hompeage.component.jsx
@@ -9,72 +9,22 @@ import Title from '../../components/title/title.component';
 //logo
 import logo from './assets/logo.png';
 //ants
-import andy from './assets/Andy.png';
-import diana from './assets/Diana.png';
-import eva from './assets/Eva.png';
-import lucy from './assets/Lucy.png';
-import mischa from './assets/Mischa.png';
-import paul from './assets/Paul.png';
-import qiu from './assets/Qiu.png';
 import crumpet from './assets/crumpet.png';
 //manifesto
 import manifesto from './assets/manifesto.png';
 //styles
 import './homepage.styles.scss';
-
-//ants
-const ants = [
-    {
-        key: 1,
-        src: andy,
-        name: "Andy Pandy",
-        title: "Play Keeper"
-    },
-    {
-        key: 2,
-        src: diana,
-        name: "Diana Vallverdu i Cabrera Bayes Bean Borras Casanovas de Luna Cabrera Baro de Dios Sampsons Cabrera Vayreda Colea Margenat Badia",
-        title: "Logic Keeper"
-    },
-    {
-        key: 3,
-        src: eva,
-        name: "Ha Lee (Eva)",
-        title: "Keeper Keeper/Orange Keeper"
-    },
-    {
-        key: 4,
-        src: lucy,
-        name: "Lucy",
-        title: "Track Keeper"
-    },
-    {
-        key: 5,
-        src: mischa,
-        name: "Mischa Jones",
-        title: "Nut Keeper"
-    },
-    {
-        key: 6,
-        src: paul,
-        name: "Paule H. Barns",
-        title: "Kind Giant, Keeper of Tech"
-    },
-    {
-        key: 7,
-        src: qiu,
-        name: "Super Qiu",
-        title: "Thrust Keeper"
-    },
-    {
-        key: 8,
-        src: crumpet,
-        name: "Qrrrrumpet",
-        title: "The Qrumpet"
-    }
-]
+//constants
+import ANTS from './homepage.data';
 
 class HomePage extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            ants: ANTS
+        }
+    }
 
     componentWillUpdate(){
         console.log('will update');
@@ -83,7 +33,7 @@ class HomePage extends Component {
     render () { return (    
     <div className="homepage">
         <RotatingHeader src={logo} alt="logo"></RotatingHeader>
-        <CardList elements={ants} title="Who We Are"></CardList>
+        <CardList elements={this.state.ants} title="Who We Are"></CardList>
         <BigImage src={manifesto} title="Our Manifesto" alt="manifesto"></BigImage>
         <Title title="Check Out Our Projects!"></Title>
         <div className="linkcrumpet">
