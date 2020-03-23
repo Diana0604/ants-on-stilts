3d7eec11137ffd6490d47fa9b8e79ef78877cc1f
Diana0604 <diana.vallverdu@gmail.com>
2020-03-23 09:30:01 +0000

    scrollt o top upon click buton

--- src/App.js
+++ src/App.js
@@ -7,13 +7,28 @@ import HomePage from './pages/homepage/hompeage.component';
 import QrumpetPage from './pages/qrumpetpage/qrumpetpage.component';
 
 class App extends Component {
+  constructor (){
+    super();
+
+    this.state = {
+      page : 'homepage'
+    };
+  }
+
+  clickQrumpetHandleClick = e => {
+    this.setState({page: 'qrumpet'});
+    window.scrollTo(0, 0);
+  }
+
+
+
   render () {
     return (
       <HashRouter basename = "/ants-on-stilts">
         <div>
           <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/qrumpet" component={QrumpetPage}/>
+            <Route exact path="/" render={() => <HomePage handleClick={this.clickQrumpetHandleClick}></HomePage>} />
+            <Route exact path="/qrumpet" component={QrumpetPage} onClick={this.clickQrumpetHandleClick}/>
           </Switch>
         </div>
       </HashRouter>
