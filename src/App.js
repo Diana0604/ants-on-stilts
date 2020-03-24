be27732138b66308f4c42532c06c3c8cbb45c533
Diana0604 <diana.vallverdu@gmail.com>
2020-03-24 11:01:25 +0000

    delete unnecessary comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//TODO -> put all (shared?) assets in src/assets instead of individual
 //TODO -> props write everything consistently ({title, blabla}) instead of (props) and use {...props} where appropriate
 import React, {Component} from 'react';
 import { Switch, Route } from "react-router-dom";
