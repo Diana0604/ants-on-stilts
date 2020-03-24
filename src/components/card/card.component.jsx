83f70b4a899512f9d238441a58e8fbce9dd7a858
Diana0604 <diana.vallverdu@gmail.com>
2020-03-24 16:59:21 +0000

    add responsiveness

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ import './card.styles.scss';
 const Card = (props) => (
     <div className="card-container">
         <img key={props.element.key} src={props.element.src} alt={props.element.alt} className="card-img"></img>
-        <h1>{props.element.title}</h1>
+        <h2>{props.element.title}</h2>
         <p>{props.element.name}</p>
     </div>
 );
